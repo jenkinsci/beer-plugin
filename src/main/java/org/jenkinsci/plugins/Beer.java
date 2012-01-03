75123ff40e1849819693012959b2328ef3003e18
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-01-03 14:37:21 -0800

    initial version

---
+++ src/main/java/org/jenkinsci/plugins/Beer.java
@@ -0,0 +1,47 @@
+package org.jenkinsci.plugins;
+
+import hudson.Extension;
+import hudson.model.UnprotectedRootAction;
+
+import java.util.Random;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+@Extension
+public class Beer implements UnprotectedRootAction {
+    private final Random random = new Random();
+
+    public String getQuote() {
+        return MESSAGES[random.nextInt(MESSAGES.length)];
+    }
+    
+    public String getImage() {
+        return IMAGES[random.nextInt(IMAGES.length)];
+    }
+    
+    public String getIconFileName() {
+        return "/plugin/beer/icon.png";
+    }
+
+    public String getDisplayName() {
+        return "We Need Beer";
+    }
+
+    public String getUrlName() {
+        return "beer";
+    }
+
+    private final String[] MESSAGES = {
+            "Having RVM problem",
+            "Maven is downloading the internet",
+            "Bundler is fetching the RubyGems index",
+            "Jenkins is restarting"
+    };
+
+    private final String[] IMAGES = {
+            "beer.gif",
+            "bigbeer.jpg",
+            "man.jpg"
+    };
+}
